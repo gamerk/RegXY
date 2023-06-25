ca863425ff383ab13581de0b7a0c029a2f52a602
Simon Kagle <merlinminosmot@gmail.com>
2023-06-25 16:54:37 -0700

    Char class ranges working

--- src/regex/char_class.c
+++ src/regex/char_class.c
@@ -8,11 +8,11 @@
 #include <stdio.h>
 
 void add_char(uint64_t (*allowed_chars)[4], char c){
-    *allowed_chars[c >> 6] |= 1ULL << abs(c & 0b00111111);
+    (*allowed_chars)[(uint8_t)c >> 6] |= 1ULL << abs(c & 0b00111111);
 }
 
 void remove_char(uint64_t (*allowed_chars)[4], char c){
-    *allowed_chars[c >> 6] &= ~(1ULL << abs(c & 0b00111111));
+    (*allowed_chars)[(uint8_t)c >> 6] &= ~(1ULL << abs(c & 0b00111111));
 }
 
 void add_char_range(uint64_t (*allowed_chars)[4], char start, char end){
@@ -27,10 +27,14 @@ void add_char_range(uint64_t (*allowed_chars)[4], char start, char end){
                 "Start and end or range must both be in 0-9, a-z, or A-Z, but got range '%c' to '%c'",
                 start, end);
     
+    ASSERT_NOT(start >= end, "Start must be before end, but start = '%c'(%1$d) and end = '%c'(%2$d)", start, end);
+    
+    uint32_t index = ((uint8_t)start) >> 6;
+    printf("Index for %c-%c: %d\n", start, end, index);
     start &= 0b00111111;
     end &= 0b00111111;
-    uint64_t u = ~0;
-    *allowed_chars[start >> 6] = ((u >> start) << start) & ~((u >> end) << end);
+    uint64_t u = ~(uint64_t)0;
+    (*allowed_chars)[index] |= ((u >> start) << start) & ~((u >> (end + 1)) << (end + 1));
 
 }
 
@@ -42,7 +46,6 @@ void add_char_class(uint64_t (*dest)[4], uint64_t src[4]){
 }
 
 bool contains_char(uint64_t allowed_chars[4], char c){
-    // printf("allowed_chars[%d](=%zX) & (1 << %d)(=%zX)\n", c >> 6, allowed_chars[c >> 6], abs(c & 0b00111111), (1ULL << abs(c & 0b00111111)));
     return (bool)(allowed_chars[((uint8_t)c) >> 6] & (1ULL << abs(c & 0b00111111)));
 }
 
@@ -75,14 +78,19 @@ ParseNode* parse_char_class(char** ptr, ParseNode* parent, bool inverted){
     *node = new_char_class((uint64_t[]){0,0,0,0}, parent, inverted);
     
     for (; **ptr != ']' && **ptr != '\0'; (*ptr)++){
-        printf("From pointer: %s\n", *ptr);
-        printf("Adding char '%c'(%1$d)\n", **ptr);
-        add_char(&(node->value.in_class), **ptr);
+        if (*(*ptr + 1) == '-'){
+            char start = **ptr;
+            char end = *(*ptr + 2);
+            add_char_range(&(node->value.in_class), start, end);
+            *ptr += 2;
+        } else {
+            add_char(&(node->value.in_class), **ptr);
+        }
     }
 
     ASSERT_NOT(**ptr == '\0', "Unclosed char class\n");
 
-    *ptr++;
+    (*ptr)++;
 
     return node;
 }
--- src/regxy.c
+++ src/regxy.c
@@ -4,13 +4,13 @@
 #include <string.h>
 #include <time.h>
 
-// gcc src/*.c src/*.h src/regex/*.c src/regex/*.h -g
+// gcc src/*.c src/*.h src/regex/*.c src/regex/*.h -g && a.exe
 
 #define PrintRange(s, start, end) {for (int _I = ((start >= 0) ? start : 0); _I < end && s[_I]; _I++) fputc(s[_I], stdout); fputc('\n', stdout);}
 
 int main(void){
 
-    ParseNode* tree = parse("[^11234221213]");
+    ParseNode* tree = parse("[aB-C]");
     print_parse_tree(tree);
     free_parse_tree(tree);
 
